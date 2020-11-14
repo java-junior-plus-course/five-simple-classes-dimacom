f6d9ff0262f2a15de7ec931ca7caf3e3b3669879
dima <karpov83d@gmail.com>
2020-11-14 05:02:21 +0300

    class Movie finished

--- src/Movie.java
+++ src/Movie.java
@@ -1,2 +1,28 @@
 public class Movie {
+    public String nameMovie, styleMovie;
+    public float ratedMovie;
+    private int numMovie;
+    public int timeMovie;
+
+    public  Movie(int num){
+        numMovie = num;
+    }
+    public int getNumMovie(){
+        return numMovie;
+    }
+    public void setNumMovie(int num){
+        numMovie = num;
+    }
+    public void choice(){
+        System.out.println("choice movie");
+    }
+    public void playMovie(){
+        System.out.println("play movie");
+    }
+    public void pauseMovie(){
+        System.out.println("Pause movie.");
+    }
+    public void stopMovie(){
+        System.out.println("Stop movie");
+    }
 }
