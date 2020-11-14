53aad41407f35de96cc5ad04797b7f9f4f115e85
dima <karpov83d@gmail.com>
2020-11-14 03:57:25 +0300

    class Phone finished

--- src/Phone.java
+++ src/Phone.java
@@ -1,2 +1,27 @@
 public class Phone {
+    public String model, colour,kit;
+    public int accumulator;
+    private int price;
+
+    public Phone(int cost){
+        price = cost;
+    }
+    public int getPrice(){
+        return price;
+    }
+    public void setPrice(int cost){
+        price=cost;
+    }
+    public void call(){
+        System.out.println("call");
+    }
+    public void msg(){
+        System.out.println("msg");
+    }
+    public void chargeUp(){
+        System.out.println("chargeUp");
+    }
+    private void reload(){
+        System.out.println("reload");
+    }
 }
